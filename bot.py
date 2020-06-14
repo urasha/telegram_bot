431b18af91792af62767593e0373c4734eeba8e8
urasha <43294860+urasha@users.noreply.github.com>
2020-06-14 10:57:40 +1000

    Working version

--- bot.py
+++ bot.py
@@ -5,46 +5,52 @@ from random import randint
 bot = telebot.TeleBot('1203791454:AAEFCUHK-e0vfvHgpsjngvmKj8hl20YfFhA')
 
 is_gaming = False
+keyboard = ''
 
 
 def start_game(message_):
     """
-    SOME COMMENT
+    Prepare the game for user:
+        1-st  ->  create keyboard with variants
+        2-nd  ->
     """
-    global is_gaming
+
+    global is_gaming, keyboard
+
+    is_gaming = True
+
     keyboard = types.ReplyKeyboardMarkup()
     keyboard.row('Rock', 'Scissors', 'Paper')
 
-    bot.send_message(message_.from_user.id, '<em><b>*The game has started now*</b></em>',
+    bot.send_message(message_.from_user.id, check_status(),
                      reply_markup=keyboard, parse_mode='html')
-    is_gaming = True
 
 
 def check_result(bot_choice_, user_choice_text_):
     """
-    SOME COMMENT
+    Checks for a win, loss, or draw
     """
     if user_choice_text_ == bot_choice_:
-        return f"Oh, it's <b>DRAW</b>. I chose {bot_choice_} too"
+        return f"Oh, it's <b>DRAW</b>. I chose {bot_choice_} too &#129309"
 
     if user_choice_text_ == 'rock' and bot_choice_ == 'paper':
-        return f"Hooray, I won, it's your <b>DEFEAT</b>. I chose {bot_choice_}"
+        return f"Hooray, I won, it's your <b>DEFEAT</b>. I chose {bot_choice_} &#128512"
 
     if user_choice_text_ == 'scissors' and bot_choice_ == 'rock':
-        return f"Hooray, I won, it's your <b>DEFEAT</b>. I chose {bot_choice_}"
+        return f"Hooray, I won, it's your <b>DEFEAT</b>. I chose {bot_choice_} &#128512"
 
     if user_choice_text_ == 'paper' and bot_choice_ == 'scissors':
-        return f"Hooray, I won, it's your <b>DEFEAT</b>. I chose {bot_choice_}"
+        return f"Hooray, I won, it's your <b>DEFEAT</b>. I chose {bot_choice_} &#128512"
 
     else:
-        return f"Huh, it's your <b>WIN</b>, bro. I chose {bot_choice_}"
+        return f"Huh, it's your <b>WIN</b>, bro. I chose {bot_choice_} &#128532"
 
 
 def show_result(user_choice):
     """
     Calculates the game result:
         1-st  ->  bot choose an action
-        2-nd  ->  verify the result of the game
+        2-nd  ->  send the result of the game
     """
 
     bot_choice = ['rock', 'scissors', 'paper'][randint(0, 2)]
@@ -54,6 +60,10 @@ def show_result(user_choice):
 
 
 def send_help_commands(message_):
+    """
+    Send all commands of the bot to user
+    """
+
     bot.send_message(message_.from_user.id,
                      """
 <b> 
@@ -64,9 +74,20 @@ def send_help_commands(message_):
 """, parse_mode='html')
 
 
-@bot.message_handler(commands=['game', 'start', 'help', 'stop'])
-def handle_game_start_help(message):
-    global is_gaming
+def check_status():
+    """
+    Checks whether the game is running or not
+    """
+
+    if not is_gaming:
+        return "<em><b>*The game hasn't started yet* &#128694</b></em>"
+
+    return "<em><b>*The game is in full swing* &#127939</b></em>"
+
+
+@bot.message_handler(commands=['game', 'start', 'help', 'stop', 'status'])
+def handle_commands(message):
+    global is_gaming, keyboard
 
     if message.text == '/game':
         start_game(message)
@@ -84,22 +105,21 @@ def handle_game_start_help(message):
 
     if message.text == '/stop':
         is_gaming = False
+        keyboard_hide = types.ReplyKeyboardRemove()
 
-        bot.send_message(message.from_user.id, "<em><b>*The game has stopped now*</b></em>", parse_mode='html')
+        bot.send_message(message.from_user.id, "<em><b>*The game has stopped now*</b></em>",
+                         reply_markup=keyboard_hide, parse_mode='html')
 
     if message.text == '/status':
-        if not is_gaming:
-            bot.send_message(message.from_user.id, "<em><b>*The game hasn't started yet* &#128694</b></em>",
-                             parse_mode='html')
-        else:
-            bot.send_message(message.from_user.id, "<em><b>*The game is in full swing &#127939*</b></em>",
-                             parse_mode='html')
+        bot.send_message(message.from_user.id, check_status(),
+                         parse_mode='html')
 
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-    if message.text.lower() in ['rock', 'scissors', 'paper'] and is_gaming:
-        show_result(message)
+    if message.text.lower() in ['rock', 'scissors', 'paper']:
+        show_result(message) if is_gaming else bot.send_message(message.from_user.id,
+                                                                "Start the game first, and then we'll play")
 
     else:
         bot.send_message(message.from_user.id, "Sorry, I don't know such commands &#128533", parse_mode='html')
